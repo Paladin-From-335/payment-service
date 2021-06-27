da544e9b421022c0c2908e0af9b749f7e40333c2
Paladin-from-335 <andrei.anufriev.work@gmail.com>
2021-06-27 22:04:19 +0300

    flyway

---
+++ src/main/resources/db/migration/V2__db_migration.sql
@@ -0,0 +1,56 @@
+create table account_table
+(
+    account_id   bigserial not null
+        constraint account_table_pk
+            primary key,
+    client_id    bigint    not null
+        constraint fkb453v9qc79nsmniqggyvwrj5i
+            references user_table,
+    account_num  bigint    not null,
+    account_type text      not null,
+    balance      numeric   not null
+);
+
+alter table account_table
+    owner to tfpxkvffnwfdmw;
+
+create unique index account_table_account_id_uindex
+    on account_table (account_id);
+
+create unique index account_table_account_num_uindex
+    on account_table (account_num);
+
+create table payment_table
+(
+    payment_id    bigserial not null
+        constraint payment_table_pk
+            primary key,
+    amount        numeric   not null,
+    reason        text,
+    timestamp     timestamp not null,
+    source_acc_id bigint    not null,
+    dest_acc_id   bigint    not null,
+    status        varchar(20) default 'NEW'::character varying
+);
+
+alter table payment_table
+    owner to tfpxkvffnwfdmw;
+
+create unique index payment_table_payment_id_uindex
+    on payment_table (payment_id);
+
+create table user_table
+(
+    client_id bigserial not null
+        constraint user_table_pk
+            primary key,
+    firstname text      not null,
+    lastname  text      not null
+);
+
+alter table user_table
+    owner to tfpxkvffnwfdmw;
+
+create unique index user_table_client_id_uindex
+    on user_table (client_id);
+
